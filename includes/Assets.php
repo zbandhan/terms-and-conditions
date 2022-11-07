9421955560672739262f6f3d4d76f793bd8d8e56
zbandhan <zbandhan@gmail.com>
2022-11-08 03:45:42 +0600

    Noting to mention

--- includes/Assets.php
+++ includes/Assets.php
@@ -2,20 +2,18 @@
 namespace Tnc;
 
 class Assets {
-    
     public function __construct() {
         add_action( 'wp_enqueue_scripts', [ $this, 'asset_style' ] );
 
     }
 
     /**
-     * Enqueuing Style
-     * 
-     * @return void 
+     * Enqueuing CSS Style
+     *
+     * @return void
      */
     public function asset_style() {
         wp_enqueue_style( 'redirect-style', plugin_dir_url(__DIR__) . 'assets/css/redirect.css' );
-
     }
 
 }
