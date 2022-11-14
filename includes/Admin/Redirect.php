b14f5b9d5505005faecc730ad71666fafefb5357
zbandhan <zbandhan@gmail.com>
2022-11-14 21:39:56 +0600

    unnecessary page checking removed

--- includes/Admin/Redirect.php
+++ includes/Admin/Redirect.php
@@ -4,8 +4,8 @@ namespace Tnc\Admin;
 class Redirect
 {
     public function __construct() {
-        add_action('admin_init', [$this, 'redirect']);
-        add_action('template_redirect', [$this, 'redirect']);
+        add_action( 'admin_init', [ $this, 'redirect_enforcing_agreement' ] );
+        add_action( 'template_redirect', [ $this, 'redirect_enforcing_agreement' ] );
     }
 
     /**
@@ -13,16 +13,14 @@ class Redirect
      *
      * @return string
      */
-    public function redirect()
-    {
-        if( ! empty( get_option( 'tnc_page') ) ) {
-            if ( ! is_page( get_option( 'tnc_page' ) )
-                && empty( $_COOKIE['tnc__agreement'] )
-                && is_user_logged_in() ) {
-                wp_safe_redirect( get_the_permalink( get_option( 'tnc_page' ) ) );
-                exit();
-            }
+    public function redirect_enforcing_agreement() {
+        if ( ! is_page( get_option( 'tnc_page' ) )
+            && empty( $_COOKIE['tnc__agreement'] )
+            && is_user_logged_in() ) {
+            wp_redirect( get_the_permalink( get_option( 'tnc_page' ) ) );
+            exit();
         }
 
     }
+
 }
